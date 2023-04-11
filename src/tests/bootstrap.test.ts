012fac1885d6b30da171e110655abeecc2942e0e
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2023-04-11 23:11:12 +0300

    fix test

--- src/tests/bootstrap.test.ts
+++ src/tests/bootstrap.test.ts
@@ -5,5 +5,5 @@ test('it should return /', async () => {
   const response = await app.inject({method: 'GET', url: '/'});
 
   expect(response.statusCode).toBe(200);
-  expect(response.payload).toBe('Hello World!');
+  expect(response.payload).toBe('Ikigai');
 })
